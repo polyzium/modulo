5759995a2b9c841e855481556271f6922dcaefc6
polyzium <polyzium@disroot.org>
2024-11-09 22:26:14 +0300

    Register commands as global

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -14,14 +14,17 @@ use std::sync::Arc;
 use serenity::{all::{Command, CommandInteraction, Context, GuildId, Http}, Error};
 
 pub async fn register_commands(http: &Arc<Http>) -> Result<Vec<Command>, Error> {
-    // // Command::create_global_command(http, ping::register()).await.unwrap();
-    // // Command::create_global_command(http, play::register()).await.unwrap();
-    // // Command::create_global_command(http, test::register()).await.unwrap();
-    // // Command::create_global_command(http, join::register()).await.unwrap();
-    // let commands = Command::get_global_commands(&http).await.unwrap();
-    // for cmd  in commands {
-    //     Command::delete_global_command(&http, cmd.id).await.unwrap();
-    // }
+    Command::create_global_command(http, play::register()).await.unwrap();
+    Command::create_global_command(http, join::register()).await.unwrap();
+    Command::create_global_command(http, leave::register()).await.unwrap();
+    Command::create_global_command(http, queue::register()).await.unwrap();
+    Command::create_global_command(http, info::register()).await.unwrap();
+    Command::create_global_command(http, jump::register()).await.unwrap();
+    Command::create_global_command(http, message::register()).await.unwrap();
+    Command::create_global_command(http, callvote::register()).await.unwrap();
+    Command::create_global_command(http, pause::register()).await.unwrap();
+    Command::create_global_command(http, resume::register()).await.unwrap();
+    Command::create_global_command(http, stop::register()).await.unwrap();
 
     /*
         To anybody who comes across this line:
@@ -30,15 +33,6 @@ pub async fn register_commands(http: &Arc<Http>) -> Result<Vec<Command>, Error> 
         Instead we make guild commands which update instantaneously.
     */
     let guild_id = GuildId::new(1302224187024216175);
-    // let guild_id = GuildId::new(683394719770083424);
-
-    // // let commands = guild_id
-    // let command_ids = guild_id.get_commands(&http).await.unwrap()
-    //     .iter_mut().map(|command| command.id)
-    //     .collect::<Vec<CommandId>>();
-    // for cmd_id  in command_ids {
-    //     guild_id.delete_command(&http, cmd_id).await.unwrap();
-    // }
     guild_id
         .set_commands(&http, vec![
             play::register(),
