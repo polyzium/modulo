e3ed1e099161bf952d81e493f10b118657451a91
polyzium <polyzium@disroot.org>
2025-03-02 20:51:57 +0300

    Refactor VC leave code

--- src/commands/leave.rs
+++ src/commands/leave.rs
@@ -2,40 +2,15 @@
 use serenity::all::{CommandInteraction, Context};
 use serenity::builder::CreateCommand;
 
-use crate::botdata::BotDataKey;
-use crate::misc::respond_command;
+use crate::misc::{leave_vc, respond_command};
 
 pub async fn handle(ctx: Context, interaction: &CommandInteraction) {
     let guild_id = interaction.guild_id.unwrap();
 
-    let manager = songbird::get(&ctx)
-        .await
-        .expect("Songbird Voice client placed in at initialisation.")
-        .clone();
-
-    if let Err(err) = manager.remove(guild_id).await {
-        match err {
-            songbird::error::JoinError::NoCall => respond_command(&ctx, interaction, "The bot is not in a voice channel").await,
-            _ => respond_command(&ctx, interaction, &("Error: ".to_owned()+&err.to_string())).await,
-        }
-        return
+    if let Err(error) = leave_vc(&ctx, guild_id).await {
+        respond_command(&ctx, interaction, &format!("{}", error)).await;
+        return;
     }
-
-    let mut lock = ctx.data.write().await;
-    let botdata = lock.get_mut::<BotDataKey>().unwrap();
-    let guild_id = interaction.guild_id.unwrap();
-
-    let session = botdata.sessions.get(&guild_id).cloned().unwrap();
-    let session_lock = session.data.write().await;
-    botdata.sessions.remove(&guild_id);
-    drop(lock);
-
-    let handle = session_lock.notification_handle.clone();
-    handle.send(crate::session::VoiceSessionNotificationMessage::Leave).await.unwrap();
-    if let Some(vote) = &session_lock.current_vote {
-        vote.timer_death_handle.send(false).await.unwrap();
-    }
-    drop(session_lock);
 
     respond_command(&ctx, interaction, "Left the voice channel").await;
 }
--- src/misc.rs
+++ src/misc.rs
@@ -1,6 +1,9 @@
 use std::{ffi::{c_char, CStr}, os::raw::c_void, time::Duration};
 
-use serenity::all::{CommandInteraction, Context, CreateInteractionResponse, CreateInteractionResponseFollowup, CreateInteractionResponseMessage};
+use serenity::all::{CommandInteraction, Context, CreateInteractionResponse, CreateInteractionResponseFollowup, CreateInteractionResponseMessage, GuildId};
+use anyhow::{anyhow, Result};
+
+use crate::botdata::BotDataKey;
 
 pub async fn respond_command(ctx: &Context, interaction: &CommandInteraction, text: &str) {
     let response = CreateInteractionResponse::Message(
@@ -52,4 +55,35 @@ pub fn escape_markdown(string: &str) -> String {
 
 pub fn format_duration(duration: Duration) -> String {
     format!("{}:{:0>2}", duration.as_secs()/60, duration.as_secs()%60)
+}
+
+pub async fn leave_vc(ctx: &Context, guild_id: GuildId) -> Result<()> {
+    let manager = songbird::get(&ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    if let Err(err) = manager.remove(guild_id).await {
+        match err {
+            songbird::error::JoinError::NoCall => return Err(anyhow!("The bot is not in a voice channel")),
+            _ => return Err(anyhow::Error::new(err).context("Error leaving the voice channel"))
+        }
+    }
+
+    let mut lock = ctx.data.write().await;
+    let botdata = lock.get_mut::<BotDataKey>().unwrap();
+
+    let session = botdata.sessions.get(&guild_id).cloned().unwrap();
+    let session_lock = session.data.write().await;
+    botdata.sessions.remove(&guild_id);
+    drop(lock);
+
+    let handle = session_lock.notification_handle.clone();
+    handle.send(crate::session::VoiceSessionNotificationMessage::Leave).await.unwrap();
+    if let Some(vote) = &session_lock.current_vote {
+        vote.timer_death_handle.send(false).await.unwrap();
+    }
+    drop(session_lock);
+
+    Ok(())
 }
