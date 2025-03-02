cf7d1e9a043d9a1230b2c6507020b76530ff9491
polyzium <polyzium@disroot.org>
2025-03-02 20:53:05 +0300

    Remove test message handler (lmao)

--- src/events.rs
+++ src/events.rs
@@ -12,14 +12,6 @@ pub struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                log::error!("Error sending message: {why:?}");
-            }
-        }
-    }
-
     async fn ready(&self, ctx: Context, botdata: Ready) {
         log::info!("Logged in as {}", botdata.user.name);
         if let Err(err) = commands::register_commands(&ctx.http).await {
